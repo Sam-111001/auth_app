5db83feeb2c8019a8c52bd3eb7ca85412e6a9cef
Shamant Myageri <code.shamant@gmail.com>
2024-10-01 12:26:16 +0530

    Added routes using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from "./pages/About"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Profile from './pages/Profile'
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignUp />} />
+        <Route path="/sign-up" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
